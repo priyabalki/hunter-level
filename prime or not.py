0bb24e450e0418bd82df6ba4aff2ebbc520bffb2
priyabalki <35560004+priyabalki@users.noreply.github.com>
2018-02-23 16:35:11 +0530

    Update prime or not.py

--- prime or not.py
+++ prime or not.py
@@ -1,10 +1,10 @@
 num=int(input())
 if num>1:
-	for i in range(2,num):
-		if(num%i==0):
-			print(num,"the number is not prime")
-			break
-	else:
-			print(num,"the number is  prime")
+for i in range(2,num):
+if(num%i==0):
+print(num,"the number is not prime")
+break
 else:
-	print("the number is not prime")
+print(num,"the number is  prime")
+else:
+print("the number is not prime")
