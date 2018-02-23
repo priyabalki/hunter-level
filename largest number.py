a04175fd6e1c72e9295b61e06bc20e354199d99f
priyabalki <35560004+priyabalki@users.noreply.github.com>
2018-02-23 12:21:54 +0530

    Create largest number.py

---
+++ largest number.py
@@ -0,0 +1,9 @@
+n1=54
+n2=80
+n3=5
+if((n1>=n2)and(n1>=n3)):
+	print("the largest number is ",n1)
+elif((n2>=n1)and(n2>=n3)):
+	print("the largest number is ",n2)
+else:
+  print("the largest number is ",n3)
