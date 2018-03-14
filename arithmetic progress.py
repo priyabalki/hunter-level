9ca8baea6fc79d1a0db3328633c703c32bd181d2
priyabalki <35560004+priyabalki@users.noreply.github.com>
2018-03-14 15:42:09 +0530

    Create arithmetic progress.py

---
+++ arithmetic progress.py
@@ -0,0 +1,11 @@
+#include<stdio.h>
+void main()
+{
+int n,a,d,l;
+float s;
+printf("enter  the  values  of  n,a,d");
+scanf("%d%d%d",&n,&a,&d);
+l=a+((n-1)*d);
+s=(n/2)*(a+1);
+printf("the  value  of  s is %d",s);
+}
