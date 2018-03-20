1f483b60f2797f02d8a0190f8a66a01c7dae3dc9
priyabalki <35560004+priyabalki@users.noreply.github.com>
2018-03-20 16:10:13 +0530

    Create abs value.py

---
+++ abs value.py
@@ -0,0 +1,14 @@
+#include<stdio.h>
+#include<conio.h>
+void main()
+{
+int h1,h2,m,m2,er,re;
+printf("enter the number");
+scanf("%d\t%d",&h1,&m1);
+printf("enter the number");
+scanf("%d\t%d",&h2,&m2);
+er=h1-h2;
+re=m1-m2;
+printf("%d\t%d",er,re);
+getch();
+}
