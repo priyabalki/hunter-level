af95e5acb0143a42e014221990dacdd64bcca95e
priyabalki <35560004+priyabalki@users.noreply.github.com>
2018-03-20 16:05:10 +0530

    Create time in minutes.py

---
+++ time in minutes.py
@@ -0,0 +1,11 @@
+#include <stdio.h>
+#include <conio.h>
+void main()
+{
+  int mins,hours,time;
+printf("time in minutes is");
+scanf("%d",&min);
+time=min/60;
+printf("time in hours is %d hours",time);
+getch();
+}
