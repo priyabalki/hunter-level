52e58dd8837f7240097729d6def74fcd364ac54d
priyabalki <35560004+priyabalki@users.noreply.github.com>
2018-03-20 16:29:04 +0530

    Create lines in para.py

---
+++ lines in para.py
@@ -0,0 +1,18 @@
+#include<stdio.h>
+#include<conio.h>
+void main()
+{
+char sen[50];
+printf("Enter paragraph");
+scanf("%s",&sen);
+int i,count=0;
+for(i=-0;s[i]!=""\n;i++)
+{
+if(s[i]==".")
+{
+count++;
+}
+}
+printf("%d",count);
+getch();
+}
