073428b4e1a0d0b3613f84c540fe45671a3225c9
priyabalki <35560004+priyabalki@users.noreply.github.com>
2018-03-21 15:56:44 +0530

    Create numeric characters.py

---
+++ numeric characters.py
@@ -0,0 +1,24 @@
+#include<stdio.h>
+#include<string.h>
+void main()
+{
+    char    str[100];
+    int countDigits;
+    int counter;
+
+    countDigits=0;
+ 
+    printf("Enter a string: ");
+    gets(str);
+ 
+    for(counter=0;str[counter]!=NULL;counter++)
+    {
+ 
+        if(str[counter]>='0' && str[counter]<='9')
+            countDigits++;
+    }
+ 
+    printf("\nDigits: %d",countDigits);
+ 
+}
+
