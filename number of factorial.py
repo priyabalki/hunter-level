c77e4ccdeb3560fa1d6cc89d02c1654725a8c8f9
priyabalki <35560004+priyabalki@users.noreply.github.com>
2018-03-21 16:33:48 +0530

    Rename factorial.py to number of factorial.py

---
+++ number of factorial.py
@@ -0,0 +1,16 @@
+#include<stdio.h>
+int main()
+{
+int n,fact,i;
+printf("enter the value");
+scanf("%d",&n);
+for(i=0;i<n;i++)
+{
+printf("the factorial of a number is");
+else
+{
+fact=fact*(n-1);
+}
+}
+return o;
+}
